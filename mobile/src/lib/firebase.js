3f4374fb034e06abd0f86748524ef79e8a716294
onatender <onatender@gmail.com>
2026-03-15 01:13:50 +0300

    Security: Move Firebase client config to environment variables

--- mobile/src/lib/firebase.js
+++ mobile/src/lib/firebase.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBfjXL8H274JvczbZfgMN2zYDVgosCMd-s",
-  authDomain: "whatdoubuy.firebaseapp.com",
-  projectId: "whatdoubuy",
-  storageBucket: "whatdoubuy.firebasestorage.app",
-  messagingSenderId: "638115613241",
-  appId: "1:638115613241:web:8ed56bf8161288a4761be8",
-  measurementId: "G-GCTQH8EHGH"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
